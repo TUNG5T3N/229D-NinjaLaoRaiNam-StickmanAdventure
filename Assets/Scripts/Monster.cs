d3f1cb70fdb61533c68bbe001ab9b496fbea4514
PBIT64 <161491653+PBIT64@users.noreply.github.com>
2026-05-02 14:02:47 +0700

    Monster Movement

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    [SerializeField] float WalkTime = 5;
+    [SerializeField] float Speed = 5;
+    
+    Rigidbody2D rigidbody2D;
+    float RoamTime = 10;
+    void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        RoamTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Time.time - RoamTime <= WalkTime)
+        {
+            SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+            bool isFlip = sprite.flipX;
+            int MoveValue = isFlip ? 1 : -1;
+            float Force = Speed * rigidbody2D.mass * MoveValue;
+            rigidbody2D.linearVelocity = new Vector2(Force, 0);
+        }
+        else
+        {
+            SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+            bool isFlip = sprite.flipX;
+            sprite.flipX = !isFlip;
+            RoamTime = Time.time;
+        }
+    }
+}
